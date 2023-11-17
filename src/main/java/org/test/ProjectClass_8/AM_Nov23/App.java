ad404ebaeead4385a91bb8635b42af93e32a2371
priyanka-2695 <priyanka.m2695@gmail.com>
2023-11-18 00:45:01 +0530

    Project08AM_Nov23

---
+++ src/main/java/org/test/ProjectClass_8/AM_Nov23/App.java
@@ -0,0 +1,13 @@
+package org.test.ProjectClass_8.AM_Nov23;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,11 @@
+package org.test;
+
+import org.junit.Test;
+
+public class Sample {
+	@Test 
+	private void test() {
+		
+	}
+
+}
